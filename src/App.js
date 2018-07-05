ebe78b0bc838ffa6b0406bcbfdaec88c615b8b08
bdemers1055 <bdemers1055@gmail.com>
2018-07-05 18:56:07 -0400

    import component colorchanger into app

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
+import Colorchanger from './Colorchanger';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      color: 'red'
-    };
-  }
-  changeColorGreen(){
-    this.setState({
-      color: 'green'
-    });
-  }
-  changeColorRed(){
-    this.setState({
-      color: 'red'
-    });
-  }
   render() {
-    const styles = { backgroundColor: this.state.color }
     return (
-      <div className = "center" >
-      <div className = "box" style = {styles} >
-      </div>
-      <button className = "left" onClick={ this.changeColorGreen.bind(this) }>Green</button>
-      <button className = "right" onClick={ this.changeColorRed.bind(this) }>Red</button>      
-      </div>
-    );
-  }
+  <div>
+  <Colorchanger />
+  </div>
+    )
+  };
 }
 
 export default App;
---
+++ src/Colorchanger.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Colorchanger extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            color: 'red'
+        };
+    }
+    changeColorGreen(){
+        this.setState({
+            color: 'green'
+        });
+    }
+    changeColorRed(){
+        this.setState({
+            color: 'red'
+        });
+    }
+    render() {
+        const styles = { backgroundColor: this.state.color } 
+        return (
+            <div className = "center" >
+            <div className = "box" style = { styles } ></div>
+            <button className = "left" onClick = {this.changeColorGreen.bind(this)}>Green</button>
+            <button className = "right" onClick = {this.changeColorRed.bind(this)}>Red</button>
+            </div>
+        )
+    }
+}
+
+export default Colorchanger;
